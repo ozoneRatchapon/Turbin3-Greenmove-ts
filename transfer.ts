887d42fddaf142b6a3bb665e2a435599f6891088
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-03-26 20:42:58 +0700

    Update transfer logic to send a fixed amount of 0.01 SOL and clean up commented code

--- transfer.ts
+++ transfer.ts
@@ -6,34 +6,38 @@ const to = new PublicKey("AqdrF1bMEayzZC72R7SxsC2KFqybT5rHPYswkFWe5Mkn");
 const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
-    try {
-      const balance = await connection.getBalance(from.publicKey);
-  
-      const transaction = new Transaction().add(
-        SystemProgram.transfer({
-          fromPubkey: from.publicKey,
-          toPubkey: to,
-          lamports: balance,
-        })
-      );
-  
-      transaction.recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
-      transaction.feePayer = from.publicKey;
-  
-      const fee = (await connection.getFeeForMessage(transaction.compileMessage(), 'confirmed')).value || 0;
-      transaction.instructions.pop();
-  
-      transaction.add(
-        SystemProgram.transfer({
-          fromPubkey: from.publicKey,
-          toPubkey: to,
-          lamports: balance - fee,
-        })
-      );
-  
-      const signature = await sendAndConfirmTransaction(connection, transaction, [from]);
-      console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${signature}?cluster=devnet`);
-    } catch (e) {
-      console.error(`Oops, something went wrong: ${e}`);
-    }
-  })();
+  try {
+    // const balance = await connection.getBalance(from.publicKey);
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        // lamports: balance,
+        lamports: LAMPORTS_PER_SOL / 100, // instruction 3) transfer 0.01 SOL
+      })
+    );
+
+    transaction.recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
+    transaction.feePayer = from.publicKey;
+
+    // const fee = (await connection.getFeeForMessage(transaction.compileMessage(), 'confirmed')).value || 0;
+    // transaction.instructions.pop();
+
+    // transaction.add(
+    //   SystemProgram.transfer({
+    //     fromPubkey: from.publicKey,
+    //     toPubkey: to,
+    //     lamports: balance - fee,
+    //   })
+    // );
+
+    // Sign transaction, broadcast, and confirm
+    const signature = await sendAndConfirmTransaction(connection, transaction, [from]);
+    console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    // Q22025 instruction 3) - https://explorer.solana.com/tx/5d2VCCitn6t7r4p5HaWFzV6GRUMPtRG4auEyqP2hcFRfsCG5MfQZsHwusYuPo6GzkTScGt9XaRnX19rgUUFeLzSG?cluster=devnet
+     
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
+})();
