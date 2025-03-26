a293b98c67d32e37f9c55ae5ed9aed8c779247ae
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-03-26 22:41:33 +0700

    Add instructions for submitting Turbin3 pre-requisites and update enrollment logic

--- enroll.ts
+++ enroll.ts
@@ -1,3 +1,6 @@
+// Instructions: 
+// 5) Submit completion of the Turbin3 pre-requisites program
+
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { Program, Wallet, AnchorProvider } from "@coral-xyz/anchor";
 import { IDL, Turbin3Prereq } from "./programs/Turbin3_prereq";
@@ -10,19 +13,21 @@ const github = Buffer.from("ozoneRatchapon", "utf8");
 const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed" });
 const program : Program<Turbin3Prereq> = new Program(IDL, provider);
 
-const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
+// Create the PDA for our enrollment account
+const enrollment_seeds = [Buffer.from("pre"), keypair.publicKey.toBuffer()];
 const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(enrollment_seeds, program.programId);
 
 (async () => {
   try {
     const txhash = await program.methods
-      .complete(github)
+      .submit(github)
       .accounts({
         signer: keypair.publicKey,
       })
       .signers([keypair])
       .rpc();
     console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+    // Q22025 - https://explorer.solana.com/tx/2htQiPvXyQXm8VgL1P9UDz1DFh2mKHmeia4ATCCE21H5AiqKQPHNZtQhx8i1BMXzfKmXsoiLPjNNBdN9kEp7p6ik?cluster=devnet
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
   }
